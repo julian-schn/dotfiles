761294807703bd55a1fb93cc303081d6e5d9fba7
julian-schn <julian@schniepp.de>
2026-03-01 15:50:42 +0100

    feat(zsh): extract aliases to separate file and add navigation shortcuts

---
+++ zsh/aliases.zsh
@@ -0,0 +1,23 @@
+# ---- Aliases ----
+alias cls=clear
+alias home='cd ~'
+
+alias scuts='echo "
+WEZTERM SHORTCUTS (macOS)
+
+PANES
+  ⌘ D            Vertical split
+  ⌘ ⇧ D          Horizontal split
+  ⌘ ⌥ ← ↑ ↓ →    Move between panes
+  ⌘ W            Close pane
+
+TABS
+  ⌘ T            New tab
+  ⌘ W            Close tab
+  ⌘ ⇧ ]          Next tab
+  ⌘ ⇧ [          Previous tab
+  ⌘ 1–9          Go to tab
+"'
+
+# ---- Functions ----
+h() { cd "$HOME/${1:-}"; }
